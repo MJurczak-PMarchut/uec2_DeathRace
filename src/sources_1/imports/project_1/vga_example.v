37b5cc1d0d5ccfb6ff1292fb1c4fc7d65f607dad
MJurczak-PMarchut <jurczak.mateusz@gmail.com>
2018-06-01 14:15:28 +0200

    TEST
    F

--- src/sources_1/imports/project_1/vga_example.v
+++ src/sources_1/imports/project_1/vga_example.v
@@ -13,10 +13,6 @@
 
 `include "verilog_macro_bus.vh"
 
-wire [`VGA_BUS_SIZE - 1 : 0] vga_bus [3:-1];
-wire [`MOUSE_BUS_SIZE - 1 : 0] mouse_bus [1:0] ;
-
-
 module vga_example (
   inout wire ps2_data,
   inout wire ps2_clk,
@@ -27,7 +23,11 @@ module vga_example (
   output reg [3:0] g,
   output reg [3:0] b,
   output wire pclk_mirror
-  );
+    );
+
+wire [`VGA_BUS_SIZE - 1 : 0] vga_bus [3:-1];
+wire [`MOUSE_BUS_SIZE - 1 : 0] mouse_bus [1:0] ;
+
 
   // Converts 100 MHz clk into 40 MHz pclk.
   // This uses a vendor specific primitive
@@ -43,42 +43,7 @@ module vga_example (
   (* KEEP = "TRUE" *) 
   (* ASYNC_REG = "TRUE" *)
   reg [7:0] safe_start = 0;
-/*
-  IBUF clk_ibuf (.I(clk),.O(clk_in));
 
-  MMCME2_BASE #(
-    .CLKIN1_PERIOD(10.000),
-    .CLKFBOUT_MULT_F(10.000),
-    .CLKOUT0_DIVIDE_F(25.000))
-  clk_in_mmcme2 (
-    .CLKIN1(clk_in),
-    .CLKOUT0(clk_out),
-    .CLKOUT0B(),
-    .CLKOUT1(),
-    .CLKOUT1B(),
-    .CLKOUT2(),
-    .CLKOUT2B(),
-    .CLKOUT3(),
-    .CLKOUT3B(),
-    .CLKOUT4(),
-    .CLKOUT5(),
-    .CLKOUT6(),
-    .CLKFBOUT(clkfb),
-    .CLKFBOUTB(),
-    .CLKFBIN(clkfb),
-    .LOCKED(locked),
-    .PWRDWN(1'b0),
-    .RST(1'b0)
-  );
-
-  BUFH clk_out_bufh (.I(clk_out),.O(clk_ss));
-  always @(posedge clk_ss) safe_start<= {safe_start[6:0],locked};
-
-  BUFGCE clk_out_bufgce (.I(clk_out),.CE(safe_start[7]),.O(pclk));
-
-  // Mirrors pclk on a pin for use by the testbench;
-  // not functionally required for this design to work.
-*/
 
 clk_wiz_0 myClk(
     .clk_out1(pclk100),
@@ -87,9 +52,6 @@ clk_wiz_0 myClk(
     .clk_out2(pclk),
     .reset(1'b0)
 );
-
-
-
 
 
   ODDR pclk_oddr (
@@ -102,74 +64,38 @@ clk_wiz_0 myClk(
     .S(1'b0)
   );
 
-  // Instantiate the vga_timing module, which is
-  // the module you are designing for this lab.
-
-  wire [11:0] rgb_rom,address;
+wire [21:0] address;
+wire [3:0] rgb_back; 
   
   
-  
-  MouseDisplayVeri myCoursor(
-    .pclk(pclk),
-    .mouse_in(mouse_bus[0]),
-    .vga_in(vga_bus[0]),
-    .vga_out(vga_bus[1])
-  );
-/*
-    image_rom my_rom(
-        .clk(!pclk),
-        .address(address),
-        .rgb(rgb_rom)
-    
-    );
-    */
-/*
-draw_rect_ctl My_rect_ctl(  
-    .clk(pclk),
-    .mouse_in(mouse_bus[0]),
-    .mouse_out(mouse_bus[1]),
-    .vsync(vga_bus[-1][35])
-);
-*/
-
-  MouseCtlVeri myMouse(
-    .clk(pclk100),
-    .ps2_clk(ps2_clk),
-    .ps2_data(ps2_data),
-    .mouse_out(mouse_bus[0])
-  );
 
   vga_timing my_timing (
     .vga_out(vga_bus[-1]),
     .pclk(pclk)
   );
   
-  start_screen my_screen(
+  draw_background my_background(
     .vga_in(vga_bus[-1]),
     .pclk(pclk),
-    .vga_out(vga_bus[0])
+    .vga_out(vga_bus[0]),
+    .address(address),
+    .rgb(rgb_back)
     );
   
-  /*  
-    draw_rect my_rect(
-        .mouse_in(mouse_bus[1]),
-        .vga_in(vga_bus[0]),
-        .pclk(pclk),
-        .vga_out(vga_bus[1]),
-        .rgb_rom(rgb_rom),
-        .address(address)
-    );
-    */
-       
- `VGA_SPLIT_INPUT(vga_bus[1])
-    
+  start_screen my_screen(
+    .address(address),
+    .clk(pclk),
+    .rgb(rgb_back)
+  );
 
-  // This is a simple test pattern generator.
+       
+ `VGA_SPLIT_INPUT(vga_bus[0])
   
   always @(posedge pclk)
   begin
     vs <= vsync_in;
     hs <= hsync_in;
+    
     {r,g,b} <= rgb_in;
   end
 
--- vivado/DeathRace.srcs/sources_1/new/start_screen.v
+++ vivado/DeathRace.srcs/sources_1/new/start_screen.v
@@ -21,33 +21,18 @@
 
 
 module start_screen(
-    input wire [`VGA_BUS_SIZE-1:0] vga_in,
-    input wire pclk,
-    output wire [`VGA_BUS_SIZE-1:0] vga_out
+    input wire clk ,
+    input wire [21:0] address,  // address = {addry[5:0], addrx[5:0]}
+    output reg [3:0] rgb
 );
 
-`VGA_SPLIT_INPUT(vga_in)
-`VGA_OUT_REG
-`VGA_MERGE_AT_OUTPUT(vga_out)
 
 reg [3:0] rom [0:120000];
 
 initial $readmemh("image_rom.data", rom); 
 
-always @(posedge pclk)
-begin
-    if(hcount_in[10:1] < 400)
-        if(vcount_in[10:1] < 300)
-                rgb_out <= (rom[hcount_in[10:1]+(vcount_in[10:1]*400)]*12'h111);
-            else
-            rgb_out <= 0;
-    hsync_out <= hsync_in;
-    vsync_out <= vsync_in;
-    hblnk_out <= hblnk_in;
-    vblnk_out <= vblnk_in;
-    hcount_out <= hcount_in;
-    vcount_out <= vcount_in;
-end
+always @*
+    rgb <= rom[address];
+                   
 
 endmodule
-
